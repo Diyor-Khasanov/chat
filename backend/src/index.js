99bcbfe09e87d690d0cc2bf79f5e2f8eb657381b
Diyor <baxacap@gmail.com>
2025-07-01 15:20:23 +0500

    deploy failure

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-avpa.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
